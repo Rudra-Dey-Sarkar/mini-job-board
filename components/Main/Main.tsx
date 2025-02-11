a82a2eaef355e01a0bd910be8e8f56b7d071e16e
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-02-11 15:01:08 +0530

    fixed

---
+++ components/Main/Main.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import { useRouter } from 'next/navigation'
+ 
+
+function Main() {
+  return (
+    <div className='w-full h-[100vh]'>
+        <img src="../../src/app/mini-job-board.png" alt="" />
+    </div>
+  )
+}
+
+export default Main
